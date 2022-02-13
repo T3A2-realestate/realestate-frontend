36c4af2c935b6b5bc021a3d2571581c37d55be22
bennychen2004181026 <77423140+bennychen2004181026@users.noreply.github.com>
2022-02-13 14:43:46 +1100

    create home page

--- frontend/realestate/src/pages/Home.jsx
+++ frontend/realestate/src/pages/Home.jsx
@@ -1,11 +1,33 @@
 import React from "react";
-
+// static asset
+import img from '../assets/img1.jpg'
+// styled component
+import { Card, Button,Form,FormControl } from "react-bootstrap";
 const Home = () => {
   return (
-    <div>
-        This a home page.
+      <div>
 
-    </div>
+<Card className="p-10 h-full w-full flex justify-center text-white">
+  <Card.Img src={img} alt="Card image" />
+  <Card.ImgOverlay>
+
+    
+    <Card.Text className="h-32"></Card.Text>
+    <Card.Text className="h-32"></Card.Text>
+    <Card.Text className="relative -left-10 decoration-8 font-black top-90 flex justify-center text-white break-words">We help you find your dream home.</Card.Text>
+    <Button className="relative -right-28 top-60 flex justify-center text-white">Broswer listings</Button>
+    <Form className="d-flex">
+        <FormControl
+          type="search"
+          placeholder="Search Suburb"
+          className="me-2 w-60 relative -right-80 -top-90"
+          aria-label="Search"
+        />
+        <Button variant="outline-success" className="relative -right-80 -top-90">Search</Button>
+        </Form>
+  </Card.ImgOverlay>
+</Card>
+</div>
   );
 };
 
